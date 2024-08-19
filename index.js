0af805d2fd76ce48dd389f3dfa485ce6a3a68866
Willian Mamede <williansilvamamede@gmail.com>
2024-08-19 15:24:22 -0300

    closes #4 - implementar as 4 operações matematicas

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 function calc(){
-    let num1 = document.getElementById("num1").value;
-    let num2 = document.getElementById("num2").value;
-
-    let result = parseFloat(num1) + parseFloat(num2);
+    document.getElementById("soma").onclick = function(){
+        let num1 = document.getElementById("num1").value;
+        let num2 = document.getElementById("num2").value;
     
-    document.getElementById("result").value = result;
+        let result = parseFloat(num1) + parseFloat(num2);
+        
+        document.getElementById("result").value = result;
+    }
+
+    document.getElementById("subtracao").onclick = function(){
+        let num1 = document.getElementById("num1").value;
+        let num2 = document.getElementById("num2").value;
+    
+        let result = parseFloat(num1) - parseFloat(num2);
+        
+        document.getElementById("result").value = result;
+    }
+
+    document.getElementById("multiply").onclick = function(){
+        let num1 = document.getElementById("num1").value;
+        let num2 = document.getElementById("num2").value;
+    
+        let result = parseFloat(num1) * parseFloat(num2);
+        
+        document.getElementById("result").value = result;
+    }
+
+    document.getElementById("divide").onclick = function(){
+        let num1 = document.getElementById("num1").value;
+        let num2 = document.getElementById("num2").value;
+    
+        let result = parseFloat(num1) / parseFloat(num2);
+        
+        document.getElementById("result").value = result;
+    }
 }
